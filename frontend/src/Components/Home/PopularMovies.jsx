9af45b684b98833df592d66daf91443c00dff75a
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-10-18 11:22:09 +0300

    style: popular movies cols-2

--- frontend/src/Components/Home/PopularMovies.jsx
+++ frontend/src/Components/Home/PopularMovies.jsx
@@ -10,7 +10,7 @@ const PopularMovies = () => {
   return (
     <div className="my-14">
       <Titles title="Popular Movies" Icon={BsCollectionFill}></Titles>
-      <div className="grid sm:mt-12 mt-6 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+      <div className="grid sm:mt-12 mt-6 xl:grid-cols-4 lg:grid-cols-3 grid-cols-2  gap-10">
         {movies?.slice(0, 8).map((movie, idx) => (
           <Movie key={idx} movie={movie}></Movie>
         ))}
--- frontend/src/Components/Movie.jsx
+++ frontend/src/Components/Movie.jsx
@@ -8,7 +8,7 @@ const Movie = ({ movie }) => {
     <>
       <div className="border border-border p-1 hover:scale-95 transitions relative rounded overflow-hidden">
         <Link to={`/movie/${movie.title}`} className="w-full">
-          <img src={movie.poster} alt={movie.title} className="w-full h-64 object-cover" />
+          <img src={movie.poster} alt={movie.title} className="w-full h-h-rate object-cover" />
         </Link>
         <div className="absolute flex-btn gap-2 bottom-0 right-0 left-0 bg-main bg-opacity-60 text-white px-4 py-3">
           <h3 className="font-semibold truncate">{movie.title}</h3>
