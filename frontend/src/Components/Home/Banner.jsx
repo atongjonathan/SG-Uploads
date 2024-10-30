d0aad8d5c508b531b0b43755700519b594b9e548
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-10-30 08:11:15 +0300

    feat: lg banner rotate

--- frontend/src/Components/Home/Banner.jsx
+++ frontend/src/Components/Home/Banner.jsx
@@ -34,22 +34,30 @@ const Banner = ({ movies }) => {
       >
         {latest?.slice(0, 6).sort(() => .5 - Math.random()).map((movie, idx) => (
           <SwiperSlide key={idx} className="relative rounded overflow-hidden">
-            <LazyLoadImage
+            <img
               src={movie.poster}
               alt={movie.title}
               className="w-full md:hidden h-100 object-cover"
             />
-            <LazyLoadImage
-              src={movie?.images[Math.floor(Math.random() * movie?.images.length)]}
+            <div style={{
+              background: `url('${movie?.poster}'`
+            }} className={`w-full md:inline-block hidden h-full object-cover blur-lg relative`}>
+
+            </div>
+            <img style={{
+              height: '130%',
+              bottom:'-80px'
+            }}
+              src={movie?.poster}
               alt={movie.title}
-              className="w-full md:inline-block hidden h-full object-cover"
+              className="absolute right-36 z-10 object-contain w-100 rotate-12 hidden lg:inline-block "
             />
-            <div className="absolute linear-bg xl:pl-32 sm:pl-32 pl-8 top-0 bottom-0 right-0 left-0 flex flex-col justify-center lg:gap-8 md:gap-5 gap-4">
+            <div className="lg:w-7/12 absolute linear-bg xl:pl-32 sm:pl-32 pl-8 top-0 bottom-0 right-0 left-0 flex flex-col justify-center lg:gap-8 md:gap-5 gap-4">
               <h1 className="xl:text-4xl truncate capitalize font-sans sm:text-2xl text-xl font-bold">{movie.title}</h1>
               <div className="flex gap-5 items-center text-dryGray">
                 <FlexMovieItems movie={movie}></FlexMovieItems>
               </div>
-              <p className='text-text text-sm'>{movie.plot}</p>
+              <p className='text-text text-sm text-left'>{movie.plot}</p>
               <div className="flex gap-5 items-center">
                 <Link
                   to={`/watch/${movie.title}`}
