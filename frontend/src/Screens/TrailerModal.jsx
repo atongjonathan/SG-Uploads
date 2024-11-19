643b1c19484700a4ecd2996cb349494633934cf7
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-11-19 19:03:08 +0300

    feat: prose

--- frontend/src/Screens/TrailerModal.jsx
+++ frontend/src/Screens/TrailerModal.jsx
@@ -24,13 +24,13 @@ export default function TrailerModal({ movie, trailer }) {
                 <PiFilmReel></PiFilmReel>
             </Button>
 
-            <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none" onClose={close}>
+            <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none w-screen" onClose={close}>
                 <DialogBackdrop className="fixed inset-0 bg-main/50" />
                 <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                     <div className="flex min-h-full items-center justify-center p-4">
                         <DialogPanel
                             transition
-                            className="w-full max-w-md rounded-xl bg-main/5 p-6 backdrop-blur-3xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+                            className="w-screen max-w-prose rounded-xl bg-main/5 p-6 backdrop-blur-3xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
                         >
                             <TrailerVideo movie={movie} trailer={trailer}></TrailerVideo>
                         </DialogPanel>
