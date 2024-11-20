1a42f6fb76d1ba9e1ceb7f8681deddb2f2453ad0
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-11-20 18:44:29 +0300

    fix: base skeleton

--- frontend/src/Screens/MyPlyrVideo.jsx
+++ frontend/src/Screens/MyPlyrVideo.jsx
@@ -2,21 +2,33 @@ import '@vidstack/react/player/styles/default/theme.css';
 import '@vidstack/react/player/styles/default/layouts/video.css';
 import { MediaPlayer, MediaProvider, Poster, Track, isYouTubeProvider } from '@vidstack/react';
 import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
+import { useEffect, useRef, useState } from 'react';
 
 
 function MyPlyrVideo({ movie }) {
 
+    const [time, setTime] = useState(null)
+    const [initialTime, setInitialTime] = useState(null)
+    const ref = useRef()
+
+    useEffect(() => {
+        console.log(initialTime)
+
+    }, [initialTime])
 
     return (<div>
-        <MediaPlayer title={movie.title} src={{ src: movie.stream, type: "video/mp4" }}            
+        <MediaPlayer title={movie.title} src={{ src: movie.stream, type: "video/mp4" }}
             viewType='video'
             logLevel='warn'
             crossOrigin
             playsInline
+            currentTime={initialTime}
             aspectRatio="16x9"
-            poster={movie?.poster} artist="SG Uploads">
+            ref={ref}
+            poster={movie?.poster} artist="SG Uploads"
+            onTimeUpdate={() => setTime(ref.current?.currentTime)}>
             <MediaProvider>
-                {(movie.captions?.length > 0 ) && <Track kind="captions" lang="en-US" src={movie.captions[0].src} label="English" default />}
+                {(movie.captions?.length > 0) && <Track kind="captions" lang="en-US" src={movie.captions[0].src} label="English" default />}
                 <Poster className="vds-poster" />
             </MediaProvider>
             <DefaultVideoLayout
@@ -29,7 +41,7 @@ export default MyPlyrVideo
 
 export function TrailerVideo({ movie, trailer }) {
 
-  
+
 
 
     function onProviderChange(provider) {
