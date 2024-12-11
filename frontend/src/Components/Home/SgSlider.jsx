009b16b10cfb2b6962bc335a5a5fff5f15f432a7
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-12-11 15:50:07 +0300

    fix: aspect ratio

--- frontend/src/Components/Home/SgSlider.jsx
+++ frontend/src/Components/Home/SgSlider.jsx
@@ -58,110 +58,100 @@ const SgSlider = ({ movies, title, Icon }) => {
   };
 
   return (
-    movies && (
-      <div className="my-16">
-        <div className="w-full flex justify-between">
-          <div className="flex sm:gap-8 gap-4 items-center truncate">
-            <Icon className="sm:w-6 sm:h-6 w-4 h-4 text-subMain" />
-            <h2 className="sm:text-xl text-lg font-semibold truncate">{title}</h2>
-          </div>
-
-          <div className="px-2 flex justify-center gap-2">
-            <Button
-              className={`transition duration-100 ease-in text-sm rounded w-7 h-7 flex-colo text-white ${
-                startDisabled ? "bg-dry" : "bg-subMain active:bg-dry"
-              }`}
-              ref={(node) => setPrevEl(node)}
-              disabled={startDisabled}
-            >
-              <FaArrowLeft />
-            </Button>
-            <Button
-              className={`transition duration-100 ease-in text-sm rounded w-7 h-7 flex-colo text-white ${
-                endDisabled ? "bg-dry" : "bg-subMain active:bg-dry"
-              }`}
-              ref={(node) => setNextEl(node)}
-              disabled={endDisabled}
-            >
-              <FaArrowRight />
-            </Button>
-          </div>
+    <div className="my-16">
+      <div className="w-full flex justify-between">
+        <div className="flex sm:gap-8 gap-4 items-center truncate">
+          <Icon className="sm:w-6 sm:h-6 w-4 h-4 text-subMain" />
+          <h2 className="sm:text-xl text-lg font-semibold truncate">{title}</h2>
         </div>
 
-        <div className="mt-10">
-          <Swiper
-            navigation={{ nextEl, prevEl }}
-            slidesPerView={3}
-            spaceBetween={40}
-            speed={500}
-            modules={[Navigation]}
-            onSlideChange={onSlideChange} // Trigger this on every slide change
-            onReachEnd={() => handleSliderChange(false, true)} // End reached
-            onReachBeginning={() => handleSliderChange(true, false)} // Beginning reached
-            breakpoints={{
-              0: {
-                slidesPerView: 2,
-                spaceBetween: 10,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-              },
-              1280: {
-                slidesPerView: 5,
-                spaceBetween: 30,
-              },
-            }}
+        <div className="px-2 flex justify-center gap-2">
+          <Button
+            className={`transition duration-100 ease-in text-sm rounded w-7 h-7 flex-colo text-white ${startDisabled ? "bg-dry" : "bg-subMain active:bg-dry"
+              }`}
+            ref={(node) => setPrevEl(node)}
+            disabled={startDisabled}
           >
-            {movies?.slice(0, 11).map((movie, idx) => (
-              <SwiperSlide
-                className="cursor-pointer"
-                key={idx}
-                onClick={() => navigate(`/watch/${movie.title}`)}
-              >
-                {isLoading ? (
-                  <Skeleton
-                    baseColor="rgb(11 15 41)"
-                    containerClassName="animate-pulse"
-                    height={270}
-                  />
-                ) : (
-                  <div className="relative group p-4 h-rate border border-border bg-dry rounded-lg overflow-hidden">
-                    {/* Movie Poster */}
-                    <div className="hover:scale-95 transitions relative rounded overflow-hidden">
-                      <Link to={`/watch/${movie.title}`} className="w-full">
-                        <LazyLoadImage
-                          effect="blur"
-                          wrapperProps={{
-                            style: { transitionDelay: "0.6s" },
-                          }}
-                          src={movie.poster}
-                          alt={movie.title}
-                          title={movie.title}
-                          className="w-full h-full object-cover"
-                        />
-                      </Link>
-                      <div className="absolute flex flex-col gap-2 bottom-0 right-0 left-0 bg-main bg-opacity-60 text-white px-4 py-3">
-                        <h6 className="font-semibold truncate">{movie.title}</h6>
-                        <div className="flex gap-2 text-star">
-                          <Rating value={movie.rating.star / 2} />
-                        </div>
-                      </div>
-                    </div>
-                    {/* Overlay Content */}
-                  </div>
-                )}
-              </SwiperSlide>
-            ))}
-          </Swiper>
+            <FaArrowLeft />
+          </Button>
+          <Button
+            className={`transition duration-100 ease-in text-sm rounded w-7 h-7 flex-colo text-white ${endDisabled ? "bg-dry" : "bg-subMain active:bg-dry"
+              }`}
+            ref={(node) => setNextEl(node)}
+            disabled={endDisabled}
+          >
+            <FaArrowRight />
+          </Button>
         </div>
       </div>
-    )
-  );
+
+      <div className="mt-10">
+        <Swiper
+          navigation={{ nextEl, prevEl }}
+          slidesPerView={3}
+          spaceBetween={40}
+          speed={500}
+          modules={[Navigation]}
+          onSlideChange={onSlideChange} // Trigger this on every slide change
+          onReachEnd={() => handleSliderChange(false, true)} // End reached
+          onReachBeginning={() => handleSliderChange(true, false)} // Beginning reached
+          breakpoints={{
+            0: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            1280: {
+              slidesPerView: 5,
+              spaceBetween: 30,
+            },
+          }}
+        >
+          {movies?.slice(0, 11).map((movie, idx) => (
+            <SwiperSlide
+              className="cursor-pointer"
+              key={idx}
+              onClick={() => navigate(`/watch/${movie.title}`)}
+            >
+              {isLoading ? (
+                <Skeleton
+                  baseColor="rgb(11 15 41)"
+                  containerClassName="animate-pulse"
+                  height={270}
+                />
+              ) : (
+                <div className="relative group p-4 border border-border bg-dry rounded-lg overflow-hidden">
+                  {/* Movie Poster */}
+                  <div className="hover:scale-95 transitions relative rounded overflow-hidden">
+                    <Link to={`/watch/${movie.title}`} className="w-full">
+                      <LazyLoadImage effect="blur" wrapperProps={{
+                        style: { transitionDelay: "0.6s" },
+                      }} src={movie.poster} alt={movie.title} title={movie.title} className="w-full aspect-[216/319]" />
+                    </Link>
+                    <div className="absolute flex flex-col gap-2 bottom-0 right-0 left-0 bg-main bg-opacity-60 text-white px-4 py-3">
+                      <h6 className="font-semibold truncate">{movie.title}</h6>
+                      <div className="flex gap-2 text-star">
+                        <Rating value={movie.rating.star / 2} />
+                      </div>
+                    </div>
+                  </div>
+                  {/* Overlay Content */}
+                </div>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  )
+
 };
 
 export default SgSlider;
