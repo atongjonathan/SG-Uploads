63f2f4108bffe5addb22be48a200e4b63904612b
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2025-02-19 18:37:07 +0300

    feat: action category

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -6,6 +6,11 @@ import SgSlider from "../Components/Home/SgSlider";
 import { BsCollectionFill, BsBookmarkStarFill } from "react-icons/bs";
 import { MovieContext } from "../context/MovieContext";
 import SiteDown from '../Screens/Error/SiteDown'
+import { PiFilmReelFill } from "react-icons/pi";
+
+
+const shuffle = (list) =>  list.sort(() => .5 - Math.random())
+
 
 const HomeScreen = () => {
   document.title = `SG Uploads | Home`;
@@ -16,16 +21,17 @@ const HomeScreen = () => {
   const sortedByDate = movies ? [...movies].sort((a, b) => new Date(b.releaseDetailed.date) - new Date(a.releaseDetailed.date)) : [];
   const sortedByRatingCount = movies ? [...movies].sort((a, b) => b.rating.count - a.rating.count) : [];
   const sortedByRatingStar = movies ? [...movies].sort((a, b) => b.rating.star - a.rating.star) : [];
+  const actionMovies = movies ? movies.filter((movie) => movie.genre.includes("Action")) : []
 
   return (
     <Layout>
       <div className="min-h-screen mb-6 lg:px-10 px-6">
         <>
-          <Banner movies={sortedByDate} />
-          <SgSlider movies={sortedByDate} title='Trending' Icon={BsCollectionFill} />
-          <SgSlider movies={sortedByRatingCount} title='Popular Movies' Icon={BsCollectionFill} />
-          <SgSlider movies={sortedByRatingStar} title='Top Rated' Icon={BsBookmarkStarFill} />
-          <Promos />
+          <Banner movies={shuffle(sortedByDate)} />
+          <SgSlider movies={shuffle(sortedByDate)} title='Trending' Icon={BsCollectionFill} />
+          <SgSlider movies={shuffle(sortedByRatingStar)} title='Top Rated' Icon={BsBookmarkStarFill} />
+          <SgSlider movies={shuffle(actionMovies)} title='Action' Icon={PiFilmReelFill} />
+          {/* <Promos /> */}
         </>
       </div>
 
