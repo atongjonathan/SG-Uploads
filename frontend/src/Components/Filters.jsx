4d0f37f893133a040335b005b09bfdb204ef7ec3
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2025-02-20 12:02:13 +0300

    style: clear btn

--- frontend/src/Components/Filters.jsx
+++ frontend/src/Components/Filters.jsx
@@ -97,7 +97,48 @@ const Filters = ({ categories }) => {
 
     return (
         <>
-            <div className="flex justify-end pt-3">
+            <div className="grid grid-cols-5 gap-7 pt-3">
+                <div className='my-4 bg-dry border text-dryGray border-gray-800 grid md:grid-cols-4 grid-cols-2 lg:gap-12 gap-2 rounded p-3 col-span-4'>
+                    {Filter.map((item, idx) => (
+                        <Listbox key={idx} value={item.value} onChange={item.onChange}>
+                            <div className="relative">
+                                <ListboxButton className='relative border border-gray-800 w-full text-white bg-main rounded-lg cursor-default py-4 pl-6 pr-10 text-left text-xs'>
+                                    <span className='block truncate'>{item.value.title}</span>
+                                    <span className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-2">
+                                        <SelectorIcon className='h-4 w-4' aria-hidden='true' />
+                                    </span>
+                                </ListboxButton>
+                                <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
+                                    <ListboxOptions className='absolute z-10 mt-1 w-full bg-white border border-gray-800 text-dryGray rounded-md shadow-lg max-h-60 py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm'>
+                                        {item.items.map((iterm, idx) => (
+                                            <ListboxOption
+                                                key={idx}
+                                                value={iterm}
+                                                className={({ active }) =>
+                                                    `relative cursor-default select-none py-2 pl-10 pr-4 
+                                            ${active ? 'bg-subMain text-white' : 'text-main'}`
+                                                }
+                                            >
+                                                {({ selected }) => (
+                                                    <>
+                                                        <span className={`block truncate ${selected ? 'font-semibold' : 'font-normal'}`}>
+                                                            {iterm.title}
+                                                        </span>
+                                                        {selected && (
+                                                            <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
+                                                                <FaCheck className='h-3 w-3' aria-hidden='true' />
+                                                            </span>
+                                                        )}
+                                                    </>
+                                                )}
+                                            </ListboxOption>
+                                        ))}
+                                    </ListboxOptions>
+                                </Transition>
+                            </div>
+                        </Listbox>
+                    ))}
+                </div>
                 <button
                     onClick={() => {
                         setYear(YearData[0])
@@ -105,52 +146,12 @@ const Filters = ({ categories }) => {
                         setTimes(TimesData[0])
                         setRates(RatesData[0])
                     }}
-                    className="flex-rows gap-3 text-white py-3 px-4 rounded border-2 border-subMain mr-2 hover:bg-subMain hover:border-main transitions"
+                    className="h-10 my-auto flex-rows col-span-1 gap-3 text-white p-3 rounded border-2 border-subMain mr-2 hover:bg-subMain hover:border-main transitions"
                 >
                     Clear
                 </button>
             </div>
-            <div className='my-4 bg-dry border text-dryGray border-gray-800 grid md:grid-cols-4 grid-cols-2 lg:gap-12 gap-2 rounded p-6'>
-                {Filter.map((item, idx) => (
-                    <Listbox key={idx} value={item.value} onChange={item.onChange}>
-                        <div className="relative">
-                            <ListboxButton className='relative border border-gray-800 w-full text-white bg-main rounded-lg cursor-default py-4 pl-6 pr-10 text-left text-xs'>
-                                <span className='block truncate'>{item.value.title}</span>
-                                <span className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-2">
-                                    <SelectorIcon className='h-4 w-4' aria-hidden='true' />
-                                </span>
-                            </ListboxButton>
-                            <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
-                                <ListboxOptions className='absolute z-10 mt-1 w-full bg-white border border-gray-800 text-dryGray rounded-md shadow-lg max-h-60 py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm'>
-                                    {item.items.map((iterm, idx) => (
-                                        <ListboxOption
-                                            key={idx}
-                                            value={iterm}
-                                            className={({ active }) =>
-                                                `relative cursor-default select-none py-2 pl-10 pr-4 
-                                            ${active ? 'bg-subMain text-white' : 'text-main'}`
-                                            }
-                                        >
-                                            {({ selected }) => (
-                                                <>
-                                                    <span className={`block truncate ${selected ? 'font-semibold' : 'font-normal'}`}>
-                                                        {iterm.title}
-                                                    </span>
-                                                    {selected && (
-                                                        <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
-                                                            <FaCheck className='h-3 w-3' aria-hidden='true' />
-                                                        </span>
-                                                    )}
-                                                </>
-                                            )}
-                                        </ListboxOption>
-                                    ))}
-                                </ListboxOptions>
-                            </Transition>
-                        </div>
-                    </Listbox>
-                ))}
-            </div>
+
         </>
 
     );
