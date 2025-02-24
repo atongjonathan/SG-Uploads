15d2f7f891f6e5bec252ca3224febf797e2c24c9
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2025-02-24 14:46:03 +0300

    fix: reviews

--- frontend/src/Components/Single/MovieRates.jsx
+++ frontend/src/Components/Single/MovieRates.jsx
@@ -4,6 +4,8 @@ import { BsBookmarkStarFill } from 'react-icons/bs'
 import Rating from '../Star'
 import Puff from "react-loading-icons/dist/esm/components/puff";
 import axios from 'axios'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid';
 
 
 const IMDB_API = import.meta.env.VITE_IMDB_API
@@ -12,8 +14,9 @@ const MovieRates = ({ movie }) => {
   const [reviews, setReviews] = useState(null)
 
   async function getReviews() {
-    const title = movie?.link?.split("/")[movie?.link.split("/")?.length - 1]
-
+    let splitted = movie?.link?.split("/")
+    const title = splitted[splitted?.length - 1]
+    console.log(title);
     const response = await axios.get(`${IMDB_API}/reviews/${title}`)
     if (response.data) {
       setReviews(response.data.reviews)
@@ -27,7 +30,7 @@ const MovieRates = ({ movie }) => {
     getReviews()
 
 
-  }, [movie])
+  }, [movie?.title])
 
   return movie && (
     <>
@@ -42,16 +45,45 @@ const MovieRates = ({ movie }) => {
             <div className="grid grid-cols-4 col-span-1 bg-main gap-6 rounded-lg md:p-12 p-6 h-header">
               {
                 reviews?.splice(0, 6).map((review, idx) => (
-                  <div key={idx} className="lg:col-span-1 col-span-2 flex flex-col gap-2 bg-dry p-2 border border-gray-800 rounded-lg align-middle text-center">
-                    <div className="flex flex-col gap-2">
-                      <h2 className='text-sm lg:text-md truncate'>{review.author}</h2>
-                      <p className="text-xs leading-6 font-medium text-text">{review.heading}</p>
-                    </div>
-                    <div className="flex flex-rows  mt-2 text-xs gap-1 text-star">
-                      <Rating value={review.stars / 2}></Rating>
-                    </div>
-                  </div>
+                  <>
+                    {
+                      review.heading && (
+                        <>
+                          {
+                            review.content ? (
+                              <div key={idx} className="lg:col-span-1 col-span-2 flex flex-col gap-1 bg-dry p-4 border border-gray-800 rounded-lg align-middle text-center h-fit">
+                                <div className="flex flex-col">
+                                  <Disclosure>
+                                    <DisclosureButton className="py-2 group  flex flex-col w-full items-center gap-1"> {review.heading}
 
+                                <ChevronDownIcon className="size-5 fill-white/60 group-data-[hover]:fill-white/50 group-data-[open]:rotate-180" />
+
+                                    </DisclosureButton>
+                                    <DisclosurePanel className="text-xs leading-6 font-medium text-text">
+                                      {review.content ?? review.heading}
+                                    </DisclosurePanel>
+                                  </Disclosure>
+                                </div>
+                                <div className="flex flex-rows  mt-2 text-xs gap-1 text-star">
+                                  <Rating value={review.stars / 2}></Rating>
+                                </div>
+                              </div>
+                            ) : (
+                              <div key={idx} className="lg:col-span-1 col-span-2 flex flex-col gap-1 bg-dry p-4 border border-gray-800 rounded-lg align-middle text-center  h-fit">
+                                <div className="flex flex-col gap-2">
+                                  <h2 className='text-sm lg:text-md truncate'>{review.author}</h2>
+                                  <p className="text-xs leading-6 font-medium text-text">{review.heading}</p>
+                                </div>
+                                <div className="flex flex-rows  mt-2 text-xs gap-1 text-star">
+                                  <Rating value={review.stars / 2}></Rating>
+                                </div>
+                              </div>
+                            )
+
+                          }</>
+                      )
+                    }
+                  </>
                 ))
               }
             </div>
