0f22d13cce5deb232596c929e7071391f37fa219
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2025-05-01 00:04:44 +0300

    fix:class

--- frontend/src/Screens/User/History.jsx
+++ frontend/src/Screens/User/History.jsx
@@ -32,35 +32,35 @@ const History = () => {
     }, []);
 
     return (
-        <div class="flex flex-col xl:px-2 gap-5">
-            <div class="flex w-full justify-between items-center gap-2 px-1 xl:px-2">
-                <span class="text-lg xl:text-xl 2xl:text-2xl font-semibold">History</span>
-                {/* <a class="ring-1 ring-gray-500 lg:ring-gray-400 hover:bg-dry px-3 rounded-full py-1 text-xs 2xl:text-sm text-gray-400 xl:font-medium" href="/history">view all</a> */}
+        <div className="flex flex-col xl:px-2 gap-5">
+            <div className="flex w-full justify-between items-center gap-2 px-1 xl:px-2">
+                <span className="text-lg xl:text-xl 2xl:text-2xl font-semibold">History</span>
+                {/* <a className="ring-1 ring-gray-500 lg:ring-gray-400 hover:bg-dry px-3 rounded-full py-1 text-xs 2xl:text-sm text-gray-400 xl:font-medium" href="/history">view all</a> */}
             </div>
             {
-                ids.length > 0 ? <div class="flex w-full overflow-y-hidden overflow-x-auto">
+                ids.length > 0 ? <div className="flex w-full overflow-y-hidden overflow-x-auto">
                     {
                         values.map((movie, idx) => (
-                            <div class="flex gap-2 w-[45%] sm:w-1/3 hover:bg-dry smoothie relative bubbly overflow-hidden rounded-xl md:w-1/4 2xl:w-1/5 flex-col shrink-0 p-2">
-                                <Link to={`/watch/${ids[idx]}`} class="aspect-video w-full relative bg-white/10 shrink-0 xl:bg-dry rounded-lg lg:rounded-xl overflow-hidden">
-                                    <span class=" lazy-load-image-background opacity"
+                            <div className="flex gap-2 w-[45%] sm:w-1/3 hover:bg-dry smoothie relative bubbly overflow-hidden rounded-xl md:w-1/4 2xl:w-1/5 flex-col shrink-0 p-2">
+                                <Link to={`/watch/${ids[idx]}`} className="aspect-video w-full relative bg-white/10 shrink-0 xl:bg-dry rounded-lg lg:rounded-xl overflow-hidden">
+                                    <span className=" lazy-load-image-background opacity"
                                     // style="color: transparent; display: inline-block; height: 100%; width: 100%;"
                                     >
-                                        <img width="100%" height="100%" src={movie.poster} class="size-full object-cover object-center !select-none shrink-0 undefined" />
+                                        <img width="100%" height="100%" src={movie.poster} className="size-full object-cover object-center !select-none shrink-0 undefined" />
                                     </span>
-                                    <div class="bg-white/20 absolute bottom-0 left-0 z-20 flex w-full ">
-                                        <hr class="!border-subMain !border-[.20rem]" style={{
+                                    <div className="bg-white/20 absolute bottom-0 left-0 z-20 flex w-full ">
+                                        <hr className="!border-subMain !border-[.20rem]" style={{
                                             width: `${movie.percentage}%`
                                         }} />
                                     </div>
 
-                                    {movie.duration && <span class="absolute bottom-[.35rem] right-1 sm:right-2 z-20 text-xs bg-black/75 sm:font-medium px-1 py-[.1rem] rounded overflow-hidden">{secondsToHHMMSS(movie.duration)}</span>}
+                                    {movie.duration && <span className="absolute bottom-[.35rem] right-1 sm:right-2 z-20 text-xs bg-black/75 sm:font-medium px-1 py-[.1rem] rounded overflow-hidden">{secondsToHHMMSS(movie.duration)}</span>}
                                 </Link>
-                                <div class="flex-grow gap-1 w-full flex pl-1 overflow-hidden">
-                                    <a class="flex flex-col flex-grow text-xs gap-1 sm:text-sm lg:text-base !leading-tight">
-                                        <div class="w-full text-[.8rem] sm:text-sm lg:text-base sm:font-medium !tracking-wide !line-clamp-2 !shrink-0 !leading-tight">{movie.title}</div>
+                                <div className="flex-grow gap-1 w-full flex pl-1 overflow-hidden">
+                                    <a className="flex flex-col flex-grow text-xs gap-1 sm:text-sm lg:text-base !leading-tight">
+                                        <div className="w-full text-[.8rem] sm:text-sm lg:text-base sm:font-medium !tracking-wide !line-clamp-2 !shrink-0 !leading-tight">{movie.title}</div>
                                     </a>
-                                    <div class="mb-auto sm:flex">
+                                    <div className="mb-auto sm:flex">
                                         <HistoryActions title={movie.title} id={ids[idx]} />
 
                                     </div>
