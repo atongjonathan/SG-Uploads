de45d7a4d6154bb13d1fccbf30e3fb0cb4bc14c6
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2025-05-29 11:14:56 +0300

    fix: default origins

--- backend/sguploads/settings.py
+++ backend/sguploads/settings.py
@@ -245,8 +245,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-CORS_ALLOWED_ORIGINS =  os.environ.get("CORS_ALLOWED_ORIGINS").split(",")
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(",")
+CORS_ALLOWED_ORIGINS =  os.environ.get("CORS_ALLOWED_ORIGINS","").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS","").split(",")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
